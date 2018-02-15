3ed80f6d4280533ac0c16a2b479945a95e74b540
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2018-02-15 22:34:23 +1100

    Start putting inbound messages on a queue to be processed

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 
 struct Server {
     running: Arc<AtomicBool>,
+    inbound_queue: Vec<Vec<u8>>,
 }
 
 impl Server {
@@ -14,34 +15,43 @@ impl Server {
     pub fn new() -> Self {
         Server {
 			running: Arc::new(AtomicBool::new(false)),
+            inbound_queue: vec![],
 		}
     }
 
     pub fn start(&mut self) {
+        self.prepare_server();
+        self.start_listening();
+        self.close_server();
+    }
+
+    fn prepare_server(&self) {
 	    println!("Starting server...");
 		let running = self.running.clone();
 		ctrlc::set_handler(move || {
             running.store(false, Ordering::SeqCst); 
         });
 	    self.running.store(true, Ordering::SeqCst);
-        self.start_listening();
-		println!("Server closed.");
+        // TODO start a thread to drain messages from inbound_queue
     }
 
     fn start_listening(&mut self) -> Result<(), Error> {
+        let mut socket = try!(UdpSocket::bind("0.0.0.0:34254"));
         while self.running.load(Ordering::SeqCst) {
-            let mut socket = try!(UdpSocket::bind("0.0.0.0:34254"));
             // read from the socket
             let mut buf = [0; 10];
             let (amt, src) = try!(socket.recv_from(&mut buf));
-
+            self.inbound_queue.push(buf.to_vec());
             // send a reply to the socket we received data from
             let buf = &mut buf[..amt];
-            let seb = String::from_utf8(buf.to_vec()).unwrap();
-            println!("{}", seb);
             try!(socket.send_to(buf, &src));
         }
         Ok(())
+    }
+
+    fn close_server(&self) {
+        // TODO close off any threads and make sure inbound_queue is empty
+	    println!("Server closed.");
     }
 
 }
